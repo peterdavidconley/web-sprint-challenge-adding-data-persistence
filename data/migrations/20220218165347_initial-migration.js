433f4a18aef5b7468cadfcbcc4d2d5bb3f89a8e5
Peter Conley <peterdavidconley@gmail.com>
2022-02-18 11:56:53 -0500

    wrote scaffolding for migration file

---
+++ data/migrations/20220218165347_initial-migration.js
@@ -0,0 +1,20 @@
+
+exports.up = async function(knex) {
+
+  await knex.schema
+  .createTable('projects')
+  .createTable('resources')
+  .createTable('tasks')
+  .createTable('project_resources')
+
+};
+
+exports.down = async function(knex) {
+  
+    await knex.schema
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('projects')
+    
+};
